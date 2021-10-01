1c9d3e744bea4831e7b4bf75619ed3b5afefe4bb
Bruno Parente <65973519+Account-onmi-2xplanet@users.noreply.github.com>
2021-10-01 08:39:21 -0300

    Create collecting data from the internet.py
    Using local python installation to import data to Power Bi from internet

---
+++ collecting data from the internet.py
@@ -0,0 +1,6 @@
+import pandas as pd
+from pandas_datareader import data as web
+import matplotlib.pyplot as plt
+dataset = web.DataReader(f'^BVSP', data_source='yahoo', start=f'02-20-2020', end=f'02-20-2021')
+dataset['Amplitude'] = dataset['High'] - dataset['Low']
+dataset['Date'] = dataset.index 
