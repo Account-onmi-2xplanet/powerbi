a0441fcd021b11f9cc2c26d8be241eaac3aa37e5
Bruno Parente <65973519+Account-onmi-2xplanet@users.noreply.github.com>
2022-10-14 05:19:54 -0300

    space added

--- collecting data from the internet.py
+++ collecting data from the internet.py
@@ -4,3 +4,4 @@ import matplotlib.pyplot as plt
 dataset = web.DataReader(f'^BVSP', data_source='yahoo', start=f'02-20-2020', end=f'02-20-2021')
 dataset['Amplitude'] = dataset['High'] - dataset['Low']
 dataset['Date'] = dataset.index 
+  
